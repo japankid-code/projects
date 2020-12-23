21d5041ab2943fcdc16e302db26aa257d3287f2c
japankid-code <73435493+japankid-code@users.noreply.github.com>
2020-12-22 22:40:00 -0600

    refactor/simplify elevator1.py

--- Udacity/lesson4/elevator/elevator1.py
+++ Udacity/lesson4/elevator/elevator1.py
@@ -2,13 +2,14 @@ import time
 import random
 
 def print_pause(string):
+    time.sleep(1)
     print(string)
-    time.sleep(0.25)
+    
 
 def intro():
     print_pause("Humanity as it was once has ceased to exist.")
     print_pause("You are a robot and have just arrived at your new assignment for the human processing plant.")
-    print_pause("You make your way directly to the elevator.")
+    print_pause("You make your way directly to the elevator and find several buttons.")
 
 intro()
 
@@ -21,17 +22,17 @@ intro()
 #         print_pause("An integer plz :)")
 
 while True:
-    print_pause("Here are your options:\n")
-    print_pause("1. Lobby\n" 
-                "2. Engineering\n" 
-                "3. Robot Resources\n")
+    print_pause("Here are your options:")
+    print_pause("1. Lobby")
+    print_pause("2. Engineering")
+    print_pause("3. Robot Resources")
     floor = input("Please enter the number for the floor you would like to visit:\n")
-    if "1" == floor:
+    if floor == "1":
         print_pause("The door closes and then opens, and you realize...")
         time.sleep(random.randrange(4))
-        print_pause("You already are in the lobby.\n")
-    elif "2" == floor:
+        print_pause("You have been in the lobby this whole time.\n")
+    if floor == "2":
         print_pause("After a moment, you find yourself in the Robot Resources department")
-    elif "3" == floor:
+    if floor == "3":
         print_pause("After a few moments, you find yourself in the Engineering department.")
     print_pause("But, this is not the right floor for you. Please choose another floor.")

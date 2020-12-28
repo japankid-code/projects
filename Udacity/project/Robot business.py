77b9e0e4272de5a8530608399dcd3b0ab4136b92
japankid-code <73435493+japankid-code@users.noreply.github.com>
2020-12-28 17:20:53 -0600

    defined upgrades function

--- Udacity/project/Robot business.py
+++ Udacity/project/Robot business.py
@@ -94,12 +94,13 @@ def science_room(items: list):
         print_pause("You make your way out of the science room.")
     elif power in items:
         print_pause("Your power upgrade allows you to force open the rusted hatch")
-        if power_level >= 0 and power_level <= 6:
+        if power_level >= 0 and power_level <= 6: # power off
             print_pause("Someone forgot to turn on the lights this morning.")
             print_pause("You flip the switch next to the hatch.")
             print_pause("Nothing happens.")
             print_pause("It seems that the power is out, you will have to investigate.")
-        if power_level >= 7 and power_level <= 10:
+        if power_level >= 7 and power_level <= 10: # power on
+            print_pause("")
 
     pick_room(items)
 
@@ -122,65 +123,84 @@ def generator_room(items: list):
 def machine_room(items: list):
     print_pause("You have made it to the machine room.")
     dotdotdot(3)
-    # you will need the power bands to be able to fix the problem in this room
+    # you will need the squirt gun to put out the fire in this room after turning the power on.
     if "power" in items:
          print_pause("Your power upgrade")
     print_pause("You make your way out of the machine room.")
     pick_room(items)
 
+def upgrades(n):
+    n = int(input("Please choose an upgrade:\n"))
+    if n == 1:
+        print_pause("Unfortunately, the wrench is out of stock at the moment.")
+    if n == 2:
+        items.append("power")
+    if n == 3:
+        items.append("squirt_gun")
+
 def tool_room(items: list):
     print_pause("You have made it to the tool room.")
     print_pause("There is a large vending machine with all kinds of upgrades.")
-    if power_level >= 0 and power_level <= 6:
+    if power_level >= 0 and power_level <= 6: # power off.
         print_pause("Unfortunately, it seems the machine is out of order")
+        print_pause("A small, dim, flashing screen shows low energy levels for the device.")
         print_pause("In order to function, power level must be increased.")
         print_pause("""
-                         ___________________
-                        |  |+==============+|   
-                        |  ||              ||
-                        |oo||    power     ||
-                        |oo||   shortage   ||
-                        |oo||              ||
-                        |oo|+==============+|
-                        |  || |==========| ||
-                        |  || |==========| ||
-                        |--|----------------| """)
-    elif power_level >= 7 and power_level <= 10:
+     ___________________
+    |  |+==============+| 
+    |  ||              ||
+    |oo||    power     ||
+    |oo||   shortage   ||
+    |oo||              ||
+    |oo||              ||
+    |oo|+==============+|
+    |  || |==========| ||
+    |  || |==========| ||
+    |--|----------------|
+    """)
+    elif power_level >= 7 and power_level <= 10: # power on.
         print_pause('''
-                             ___________________
-                            |  |+==============+|
-                            |  ||              ||
-                            |  || 1 wrench     ||
-                            |  || 2 power fist ||
-                            |  || 3 squirter   ||
-                            |  ||              ||
-                            |  |+==============+|
-                            |  || |==========| ||
-                            |  || |==========| ||
-                            |__|----------------|''')
-        print_pause("You are able to choose one upgrade, here are your options:")
-        print_pause("1. Wrench attachment, quite handy\n"
-                    "2. Power adapter, for robot stuff\n" 
-                    "3. Squirt gun, all kinds of uses")
-        first_upgrade = input("Please choose an upgrade:\n")
-        if first_upgrade == "1":
-            items.append("wrench")
-        if first_upgrade == "2":
-            items.append("power")
-        if first_upgrade == "3":
-            items.append("squirt_gun")
-        if "assistant" in items:
-            print_pause("Your assistant status entitles you to some credit toward an item.")
-            
-            second_upgrade = input("Please choose an upgrade:\n")
-            if first_upgrade == "1":
-                items.append("wrench")
-            if first_upgrade == "2":
-                items.append("power")
-            if first_upgrade == "3":
-                items.append("squirt_gun")
-            if "administrator" in items:
-                second_upgrade = 
+     ___________________
+    |  |+==============+|
+    |  ||              ||
+    |oo|| 1 wrench     ||
+    |oo|| 2 power fist ||
+    |oo|| 3 squirter   ||
+    |oo||              ||
+    |  |+==============+|
+    |  || |==========| ||
+    |  || |==========| ||
+    |__|----------------|
+    ''')
+        print_pause("You interface with the machine and realize it is trying to tell you something...")
+        print_pause("The little guy would like to thank you for turning the power back on!")
+        print_pause("You are granted one free upgrade! here are your options:")
+        print_pause("1. Wrench attachment, quite handy.\n"
+                    "2. Power adapter, for robot stuff.\n" 
+                    "3. Squirt gun, all kinds of uses.")
+        upgrades(first)
+        if assistant in items:
+            print_pause("Your assistant status entitles you to some credit toward an item as well.")
+            upgrades(second)
+        
+        # first_upgrade = input("Please choose an upgrade:\n")
+        # if first_upgrade == "1":
+        #     items.append("wrench")
+        # if first_upgrade == "2":
+        #     items.append("power")
+        # if first_upgrade == "3":
+        #     items.append("squirt_gun")
+        # if "assistant" in items:
+        #     print_pause("Your assistant status entitles you to some credit toward an item.")
+        #     second_upgrade = input("Please choose an upgrade:\n")
+        #     if first_upgrade == "1":
+        #         items.append("wrench")
+        #     if first_upgrade == "2":
+        #         items.append("power")
+        #     if first_upgrade == "3":
+        #         items.append("squirt_gun")
+        #     if "administrator" in items:
+        #         second_upgrade = 
             
     print(items)
     print_pause("You make your way out of the tool room.")
@@ -210,20 +230,20 @@ def pick_room(items: list):
     room_chooser(items)   
 
 def part_two(items: list):
-    choice = input("would you like to continue playing? (y/n):\n")
-    if choice == "y":
-        print_pause("Finally, your first day at your new job can begin.")
-        print_pause("You make your way out of the front recieving area into a long hallway.")
-        print_pause("You observe in the dimly lit corridor that there is a map on the wall.")
-        print_pause("You study the map, your sensors memorizing the options.")
-        print_pause("You turn to continue down the hallway.")
-        pick_room(items)
-    elif choice == "n":
-        print_pause("Program terminated.")
-        exit()
-    else:
-        print_pause("I did not understand, please re-enter response:\n")
-        part_two(items)
+    # choice = input("would you like to continue playing? (y/n):\n")
+    #if choice == "y":
+    print_pause("Finally, your first day at your new job can begin.")
+    print_pause("You make your way out of the front recieving area into a long hallway.")
+    print_pause("You observe in the dimly lit corridor that there is a map on the wall.")
+    print_pause("You study the map, your sensors memorizing the options.")
+    print_pause("You turn to continue down the hallway.")
+    pick_room(items)
+    # elif choice == "n":
+    #     print_pause("Program terminated.")
+    #     exit()
+    # else:
+    #     print_pause("I did not understand, please re-enter response:\n")
+    #     part_two(items)
 
 def play_game():
     items = []
@@ -233,7 +253,7 @@ def play_game():
 
 def shortcut():
     global power_level
-    power_level = "0"
+    power_level = 0
     items = ["handbook", "ID chip", "assistant"]
     part_two(items)
 

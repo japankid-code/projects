81732c8e39cb6700141f377cd37e69545614d4cd
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-02 14:56:55 -0600

    added results.html, updated index.html

--- Python/Django/src/polls/urls.py
+++ Python/Django/src/polls/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.detail, name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]

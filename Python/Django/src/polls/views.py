246db2ddcbf83a26b555a876faf4be84e5c0f8bd
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-01 23:01:03 -0600

    django site, add polls app

---
+++ Python/Django/src/polls/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello world.\nYou're at the polls index.")

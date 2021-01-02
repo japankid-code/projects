cc98505b8632088064dca28152a7e9a43a452486
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-02 15:10:07 -0600

    updated imports on views, changed urls.py to match classes in views

--- Python/Django/src/polls/views.py
+++ Python/Django/src/polls/views.py
@@ -1,10 +1,9 @@
-from django.http.response import Http404
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 from django.views import generic
 
-from .models import Question
+from .models import Choice, Question
 
 # Create your views here.
 

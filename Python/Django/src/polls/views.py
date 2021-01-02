6048018c73b6370edc95f70652eb1ff20641581c
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-02 14:00:50 -0600

    created template subdirectory
    namespacing the templates folders for this app so django doesn't get confused and pick the wrong one

--- Python/Django/src/polls/views.py
+++ Python/Django/src/polls/views.py
@@ -12,12 +12,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    #    try:
-    #        question = Question.objects.get(pk=question_id)
-    #    except Question.DoesNotExist:
-    #        raise Http404("Question does not exist.")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist.")
     # gets shortened to:
-    question = get_object_or_404(Question, pk=question_id)
+    # question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 

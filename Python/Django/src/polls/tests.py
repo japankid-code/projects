434d1bf936a3d4e101a32358a62aacde6384751f
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-04 19:50:19 -0600

    updated question choices, test still fails

--- Python/Django/src/polls/tests.py
+++ Python/Django/src/polls/tests.py
@@ -5,7 +5,6 @@ from django.utils import timezone
 
 from .models import Question
 
-# Create your tests here.
 
 class QuestionModelTests(TestCase):
 
@@ -17,3 +16,4 @@ class QuestionModelTests(TestCase):
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
+        

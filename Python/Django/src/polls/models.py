413d7718fabf3e23a8bbdb4b9c973377f258344e
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-04 20:25:30 -0600

    fixed models.py, 3 passing tests :)

--- Python/Django/src/polls/models.py
+++ Python/Django/src/polls/models.py
@@ -9,7 +9,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
@@ -18,8 +19,3 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
-
-
-def was_published_recently(self):
-    now = timezone.now()
-    return now - datetime.timedelta(days=1) <= self.pub_date <= now

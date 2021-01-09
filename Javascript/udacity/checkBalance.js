c1294db94525ebc5cf0d6026ef2823fe8d093792
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-09 16:27:17 -0600

    Create checkBalance.js

---
+++ Javascript/udacity/checkBalance.js
@@ -0,0 +1,26 @@
+/*
+ * Programming Quiz - Checking Your Balance (3-5)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have the variables `balance`, `checkBalance`, `isActive`
+ * 2. Your code should include an `if...else` conditional statement
+ * 3. Your code should produce the expected output
+ * 4. Your code should not be empty
+ * 5. BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED. 
+ */
+ 
+// change the values of `balance`, `checkBalance`, and `isActive` to test your code
+var balance = 325.00;
+var checkBalance = true;
+var isActive = false;
+
+// your code goes here
+if (checkBalance === true) {
+    if (isActive === false)
+        console.log("Your account is no longer active.")
+} else if (checkBalance === true) {
+    if (isActive === true)
+        balance.toFixed(2)
+}

4d8e28412425e053ee512f96194d3522f21db10f
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-09 16:56:19 -0600

    Create shirtSizer.js

---
+++ Javascript/udacity/shirtSizer.js
@@ -0,0 +1,43 @@
+/*
+ * Programming Quiz: What do I Wear? (3-7)
+ *
+ * Using if/else statements, create a series of logical expressions that logs the size of a t-shirt based on the measurements of:
+ *   - shirtWidth
+ *   - shirtLength
+ *   - shirtSleeve
+ *
+ * Use the chart above to print out one of the following correct values:
+ *   - S, M, L, XL, 2XL, or 3XL
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have the variables `shirtWidth`, `shirtLength`, and `shirtSleeve`
+ * 2. Your code should include an `if...else` conditional statement
+ * 3. Your code should use logical expressions
+ * 4. Your code should produce the expected output
+ * 6. Your code should not be empty
+ * 7. BE CAREFUL ABOUT THE EXACT CHARACTERS TO BE PRINTED.
+ */
+ 
+
+
+// change the values of `shirtWidth`, `shirtLength`, and `shirtSleeve` to test your code
+var shirtWidth = 23;
+var shirtLength = 30;
+var shirtSleeve = 8.71;
+
+/*
+ * To gain confidence, check your code for the following combination of [shirtWidth, shirtLength, shirtSleeve, expectedSize]:
+ * [18, 28, 8.13, 'S']
+ * [19.99, 28.99, 8.379, 'S']
+ * [20, 29, 8.38, 'M']
+ * [22, 30, 8.63, 'L']
+ * [24, 31, 8.88, 'XL']
+ * [26, 33, 9.63, '2XL']
+ * [27.99, 33.99, 10.129, '2XL']
+ * [28, 34, 10.13, '3XL']
+ * [18, 29, 8.47, 'NA']
+*/
+
+// WRITE YOUR CODE HERE

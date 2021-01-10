669020de477aee120ea04bc178ace5f1e8c1dbf8
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-10 13:45:44 -0600

    Update script.js

--- Javascript/udacity/script.js
+++ Javascript/udacity/script.js
@@ -1,12 +1,17 @@
+//
+var education = "a Bachelor's degree";
+// set the value of this based on a person's education
+var salary = 0;
+
 switch (education) {
-    case 'no high school diploma':
+    case "no high school diploma":
         salary = 25636;
         break;
-    case 'a high school diploma' :
-        salary = 35,256;
+    case "a high school diploma":
+        salary = 35256;
         break;
     case "an Associate's degree":
-        salary = 41,496;
+        salary = 41496;
         break;
     case "a Bachelor's degree":
         salary = 59124;
@@ -21,4 +26,6 @@ switch (education) {
         salary = 84396;
         break;
 }
-console.log("In 2015, a person with "+education+" earned an average of $"+salary+"/year.")
+
+
+console.log("In 2015, a person with "+education+" earned an average of $"+salary.toLocaleString("en-US")+"/year.");

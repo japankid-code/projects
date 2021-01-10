7f51a14e9761727586b53c843eb6b5bb03cc3512
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-09 20:01:46 -0600

    Update shirtSizer.js

--- Javascript/udacity/shirtSizer/shirtSizer.js
+++ Javascript/udacity/shirtSizer/shirtSizer.js
@@ -15,7 +15,7 @@
  * 1. Your code should have the variables `shirtWidth`, `shirtLength`, and `shirtSleeve`
  * 2. Your code should include an `if...else` conditional statement
  * 3. Your code should use logical expressions
- * 4. Your code should produce the expected output
+ * 4. Your code should produce the  output
  * 6. Your code should not be empty
  * 7. BE CAREFUL ABOUT THE EXACT CHARACTERS TO BE PRINTED.
  */
@@ -23,12 +23,12 @@
 
 
 // change the values of `shirtWidth`, `shirtLength`, and `shirtSleeve` to test your code
-var shirtWidth = 23;
-var shirtLength = 30;
-var shirtSleeve = 8.71;
+var shirtWidth = 18;
+var shirtLength = 29;
+var shirtSleeve = 8.47;
 
 /*
- * To gain confidence, check your code for the following combination of [shirtWidth, shirtLength, shirtSleeve, expectedSize]:
+ * To gain confidence, check your code for the following combination of [shirtWidth, shirtLength, shirtSleeve, size]:
  * [18, 28, 8.13, 'S']
  * [19.99, 28.99, 8.379, 'S']
  * [20, 29, 8.38, 'M']
@@ -42,24 +42,27 @@ var shirtSleeve = 8.71;
 
 // WRITE YOUR CODE HERE
 
-[shirtWidth, shirtLength, shirtSleeve, expectedSize]
+var size = "NA";
 
-if ((shirtWidth>=18) && (shirtWidth<20) && (shirtLength>=28) && (shirtLength<29) && (shirtSleeve>=7) && (shirtSleeve<8.13)) {
-    console.log("S")
+if ((shirtWidth>=18 && shirtWidth<20) && (shirtLength>=28 && shirtLength<29) && (shirtSleeve>=8.13 && shirtSleeve<8.38) ) {
+    size = "S";
 }
-if ((shirtWidth>=20) && (shirtWidth<22) && (shirtLength>=29) && (shirtLength<30) && (shirtSleeve>=8.38) && (shirtSleeve<8.63)) {
-    console.log("M")
+else if ((shirtWidth>=20 && shirtWidth<22) && (shirtLength>=29 && shirtLength<30) && (shirtSleeve>=8.38 && shirtSleeve<8.63) ) {
+    size = "M";
 }
-if ((shirtWidth>=22) && (shirtWidth<24) && (shirtLength>=31) && (shirtLength<32) && (shirtSleeve>=8.63) && (shirtSleeve<8.88)) {
-    console.log("L")
+else if ((shirtWidth>=22 && shirtWidth<24) && (shirtLength>=30 && shirtLength<31) && (shirtSleeve>=8.63 && shirtSleeve<8.88) ) {
+    size = "L";
 }
-if ((shirtWidth>=24) && (shirtWidth<26) && (shirtLength>=32) && (shirtLength<33) && (shirtSleeve>=8.88) && (shirtSleeve<9.63)) {
-    console.log("XL")
+else if ((shirtWidth>=24 && shirtWidth<26) && (shirtLength>=31 && shirtLength<33) && (shirtSleeve>=8.88 && shirtSleeve<9.63) ) {
+    size = "XL";
 }
-if ((shirtWidth>=26) && (shirtWidth<28) && (shirtLength>=33) && (shirtLength<34) && (shirtSleeve>=8.13) && (shirtSleeve<9.63)) {
-    console.log("2XL")
+else if ((shirtWidth>=26 && shirtWidth<28) && (shirtLength>=33 && shirtLength<34) && (shirtSleeve>=9.63 && shirtSleeve<10.13) ) {
+    size = "2XL";
 }
-if ((shirtWidth>=28) && (shirtWidth<32) && (shirtLength>=34) && (shirtLength<35) && (shirtSleeve>=10.13) && (shirtSleeve<11)) {
-    console.log("3XL")
+else if ((shirtWidth>=28) && (shirtLength>=34) && (shirtSleeve>=10.13) ) {
+    size = "3XL";
 }
-
+else {
+    size = "NA";
+}
+console.log(size);

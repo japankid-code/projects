8bf8ac6404705be7c9a43a0a6f3fdd0d7125d204
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-11 18:28:44 -0600

    Update fizzbuzz.js, nest fizzbuzz to shorten code

--- Javascript/udacity/fizzbuzz.js
+++ Javascript/udacity/fizzbuzz.js
@@ -1,11 +1,10 @@
 var x = 1;
 
 while (x <= 20) {
-    if (x % 3 === 0 && x % 5 === 0) {
-        console.log("fizzbuzz");
-    }else if (x % 5 === 0) {
-        console.log("buzz");
-    }else if (x % 3 === 0) {
+    if (x % 3 === 0) {
+        console.log("fizz");
+        if (x % 5 === 0) {console.log("fizzbuzz")}
+    } else if (x % 5 === 0) {
         console.log("fizz");
     }
     else {

b93e380b3f560673972d84470dfe9cfb18a59797
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-11 18:27:22 -0600

    Create fizzbuzz.js

---
+++ Javascript/udacity/fizzbuzz.js
@@ -0,0 +1,15 @@
+var x = 1;
+
+while (x <= 20) {
+    if (x % 3 === 0 && x % 5 === 0) {
+        console.log("fizzbuzz");
+    }else if (x % 5 === 0) {
+        console.log("buzz");
+    }else if (x % 3 === 0) {
+        console.log("fizz");
+    }
+    else {
+        console.log(x);
+    }
+    x += 1
+}

dbd2233eae365d41b2123cdc96a3cead3b68ee27
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-23 21:31:44 -0600

    added choices to admin site

--- Python/Django/src/polls/admin.py
+++ Python/Django/src/polls/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,                  {'fields': ['question_text']}),
+        ('Date Information',    {'fields': ['pub_date']}),
+    ]
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)

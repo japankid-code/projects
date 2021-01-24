c0785bd0c0919e01d76024519860e8819028f2b4
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-23 23:12:21 -0600

    implement addTodo

--- Python/Django/src/todo/views.py
+++ Python/Django/src/todo/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'todo/index.html', context)
+
+def addTodo(request):
+    form = TodoForm(request.POST)
+
+    if form.is_valid():
+        new_todo = Todo(text=request.POST['text'])
+        new_todo.save()
+
+    return redirect('index')

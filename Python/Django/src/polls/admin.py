2b9fdb48818ddc68e50e8e567fb4151ea6f11873
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-23 21:43:35 -0600

    add in line choices to questions admin page, not working ??

--- Python/Django/src/polls/admin.py
+++ Python/Django/src/polls/admin.py
@@ -4,11 +4,16 @@ from .models import Choice, Question
 
 # Register your models here.
 
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,                  {'fields': ['question_text']}),
-        ('Date Information',    {'fields': ['pub_date']}),
+        ('Date Information',    {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
+    inLines = [ChoiceInLine]
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)

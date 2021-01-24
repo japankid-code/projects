9ec830c1f711c5c5897e8dbcf67d958d31b1fbd5
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-23 21:24:11 -0600

    changed order of pub date and question text on admin page

--- Python/Django/src/polls/admin.py
+++ Python/Django/src/polls/admin.py
@@ -4,4 +4,7 @@ from .models import Question
 
 # Register your models here.
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)

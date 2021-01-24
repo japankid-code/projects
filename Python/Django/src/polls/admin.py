29edaf076465d5eafe4e2d7065c0b251bfb619c3
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-23 21:54:08 -0600

    Update admin.py, got inline questions working on admin page

--- Python/Django/src/polls/admin.py
+++ Python/Django/src/polls/admin.py
@@ -2,18 +2,17 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
-# Register your models here.
 
-class ChoiceInLine(admin.StackedInline):
+class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 3
 
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,                  {'fields': ['question_text']}),
-        ('Date Information',    {'fields': ['pub_date'], 'classes': ['collapse']}),
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
-    inLines = [ChoiceInLine]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)

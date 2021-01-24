a14be18395c92ddc23405f225a61b0ab43a1746e
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-23 23:27:20 -0600

    add a Todo form. add todo fields to models

--- Python/Django/src/todo/models.py
+++ Python/Django/src/todo/models.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request, 'todo/index.html', context)
+
+class Todo(models.Model):
+    text = models.CharField(max_length=40)
+    complete = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text

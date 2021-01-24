ca15e57c3d818af10a1d01a5ec0c19c3e7e7661a
japankid-code <73435493+japankid-code@users.noreply.github.com>
2021-01-23 23:13:28 -0600

    add completetodo attribute in views.py

--- Python/Django/src/todo/views.py
+++ Python/Django/src/todo/views.py
@@ -4,11 +4,20 @@ def index(request):
     context = {}
     return render(request, 'todo/index.html', context)
 
+
 def addTodo(request):
     form = TodoForm(request.POST)
 
     if form.is_valid():
         new_todo = Todo(text=request.POST['text'])
         new_todo.save()
+
+    return redirect('index')
+
+
+def completeTodo(request, todo_id):
+    todo = Todo.objects.get(pk=todo_id)
+    todo.complete = True
+    todo.save()
 
     return redirect('index')

3d8e7d53e1c3c8f64423b18437c270a09f8481ca
japankid-code <japankid.jake@gmail.com>
2021-02-13 13:02:05 -0600

    Update index.js, XOXO
    added to the the initial state in the Board constructor.
    modified HandleClick to flip flop the value of that state.
    updated the status text above the board.

--- reactworkspace/tictactoe/src/index.js
+++ reactworkspace/tictactoe/src/index.js
@@ -17,13 +17,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null), // sets init state, array of 9 nulls corresponding to 9 sq
+      xIsNext: true,
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -37,7 +41,7 @@ class Board extends React.Component {
   };
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>

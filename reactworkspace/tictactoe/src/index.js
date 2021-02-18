55efa498d67c4e341f83ac78caba6703098791a5
japankid-code <japankid.jake@gmail.com>
2021-02-17 21:44:04 -0600

    implement time travel
    added step number to init state.
    added jumpto function, which updates the step number (because we had the button onClick modified in the last commit).
    handleclick's setState now updates the step number

--- reactworkspace/tictactoe/src/index.js
+++ reactworkspace/tictactoe/src/index.js
@@ -53,24 +53,33 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return; //this pauses inputs after game finish
-    } 
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     console.log(this.state);
     this.setState({
       history: history.concat([{
         squares: squares
       }]),
-      xIsNext: !this.state.xIsNext
+      stepNumber: history.length,
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0, // sets true if stepnumber is even.
     });
   }
 
